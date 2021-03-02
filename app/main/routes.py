e4fabb9a84e972d946a6b384bdb4c7f185bf1aa7
johnnyquinn <jftq97@gmail.com>
2021-03-02 00:12:17 -0500

    [add] templates, intial code for routes, intial code for models, and completed (at least it should be) __init__.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
 
 main = Blueprint('main', __name__)
 
-# Create your routes here.
+bcrypt = Bcrypt(app)
+
+
+@main.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/logout')
+def logout():
+    return redirect(url_for('main.homepage'))
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@main.route('/beer_submit')
+def beer_submit():
+    return render_template('beer_submit.html')
+
+@main.route('/beer_review')
+def beer_review():
+    return render_template('beer_review.html')
+
+@main.route('/beer_detail')
+def beer_detail():
+    return render_template('beer_detail.html')
--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
 # Create your models here.
+from app import db 
+
+class User(db.Model):
+    """User Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
+class Beer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    brand = db.Column(db.String(20))
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String(30))
+
+
